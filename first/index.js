4a201c4fe914d75c4e4bfbfb1ad98233d27fde77
sreegopalsaha <sreegopal0101@gmail.com>
2025-04-22 01:15:35 +0530

    initialize project with essential files and dependencies

---
+++ first/index.js
@@ -0,0 +1,33 @@
+import { GoogleGenAI } from "@google/genai";
+import "dotenv/config";
+
+// https://googleapis.github.io/js-genai/main/index.html
+
+if (!process.env.GEMINI_API_KEY) {
+  console.log("GOOGLE_API_KEY is not set in .env file");
+  process.exit(1);
+}
+
+const model = "gemini-2.0-flash-001";
+const contents = [
+  {
+    parts: [
+      {
+        text: "Hello, how are you?",
+      },
+    ],
+    role: "user",
+  },
+];
+
+config = {};
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+const response = await ai.models.generateContent({
+  model,
+  config,
+  contents,
+});
+
+console.log(response.text);
